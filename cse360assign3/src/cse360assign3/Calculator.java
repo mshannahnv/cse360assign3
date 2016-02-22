28941bbc2c4c9f23d6cac0e5e6da854be63d92c8
Hannah <mshannahnv@gmail.com>
2016-02-22 07:51:52 -0700

    Initial commit of Calculator & CalculatorTest.

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,63 @@
+/** Calculator performs some basic mathematical calculations.
+ *
+ * @author Hannah Van Den Bosch (826)
+ * @version February 22 2016
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+    private int total;
+
+    /** Constructor */
+    public Calculator() {
+        total = 0; // not needed - included for clarity
+    }
+
+    /** getTotal returns the total variable.
+     * 
+     * @return  Total value. */
+    public int getTotal() {
+        return 0;
+    }
+
+    /** add adds the parameter to the total variable.
+     *
+     * @param   value Amount to be added to the total.
+     */
+    public void add(int value) {
+
+    }
+
+    /** subtract subtracts the parameter from the total variable.
+     *
+     * @param   value Amount to be subtracted from the total.
+     */
+    public void subtract(int value) {
+
+    }
+
+    /** multiply multiplies the total by the parameter.
+     *
+     * @param   value Amount to be multiplied by the total.
+     */
+    public void multiply(int value) {
+
+    }
+
+    /** divide divides the total by the parameter.
+    *
+    * @param    value Amount to be divided by the total.
+    */
+    public void divide(int value) {
+
+    }
+
+    /** getHistory returns an empty String.
+     * 
+     * @return  Empty String. */
+    public String getHistory() {
+        return "";
+    }
+}
